d26bc03e85d58d2a0761558afc3b0b2a347a8255
LOrence <lawrence.chandru@gmail.com>
2021-07-21 23:48:38 +1200

    update

---
+++ POM_Task2_DataDriven/Pages/ChangePasswordPage.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using POM_Task2_DataDriven.Utilities;
+using static POM_Task2_DataDriven.Utilities.CommonMethods;
+
+namespace POM_Task2_DataDriven.Pages
+{
+    public class ChangePasswordPage
+    {
+        private readonly IWebDriver driver;
+        private SignInPage signIn;
+
+        //page factory design pattern
+        IWebElement Username => driver.FindElement(By.XPath("//span[contains(text(),'Hi')]"));
+        IWebElement ChangePasswordItem => driver.FindElement(By.XPath("//a[text()='Change Password']"));
+        IWebElement CurrentPassword => driver.FindElement(By.XPath("//input[@name='oldPassword']"));
+        IWebElement NewPassword => driver.FindElement(By.XPath("//input[@name='newPassword']"));
+        IWebElement ConfirmPassword => driver.FindElement(By.XPath("//input[@name='confirmPassword']"));
+        IWebElement Save => driver.FindElement(By.XPath("//button[@class='ui button ui teal button' and text()='Save']"));
+        IWebElement Message => driver.FindElement(By.XPath("//div[contains(text(),'Password Changed Successfully')]"));
+
+        //Create a Constructor
+        public ChangePasswordPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            signIn = new SignInPage(driver);
+        }
+
+        //changing password
+        public void ChangePassword()
+        {
+            signIn.Login(ExcelLibHelper.ReadData(1, "EmailAddress"), ExcelLibHelper.ReadData(1, "Password"));
+            ClickChangePassword();
+            ValidateYouAreAtChangePasswordPage();
+            EnterData();
+            ClickSave();
+            bool isPasswordChanged = ValidateSuccessMessage();
+            Assert.IsTrue(isPasswordChanged);
+
+        }
+
+
+        public void ClickChangePassword()
+        {
+            Wait.ElementExists(driver, "XPath", "//span[contains(text(),'Hi')]", 50);
+
+            //click Hi username
+            Username.Click();
+
+            Thread.Sleep(200);
+
+            Wait.ElementExists(driver, "XPath", "//a[text()='Change Password']", 50);
+
+            //click Change Password menu item
+            ChangePasswordItem.Click();
+
+        }
+
+        public void ValidateYouAreAtChangePasswordPage()
+        {
+            Wait.ElementExists(driver, "XPath", "//button[@class='ui button ui teal button' and text()='Save']", 300);
+            bool isChangePasswordPage = Save.Displayed;
+            Assert.IsTrue(isChangePasswordPage);
+
+        }
+
+        public void EnterData()
+        {
+
+            try
+            {
+                Wait.ElementExists(driver, "XPath", "//input[@name='oldPassword']", 50);
+                //Enter current password
+                CurrentPassword.SendKeys(ExcelLibHelper.ReadData(1, "CurrentPassword"));
+
+                //Enter new password
+                NewPassword.SendKeys(ExcelLibHelper.ReadData(1, "NewPassword"));
+
+                //Enter confirm password
+                ConfirmPassword.SendKeys(ExcelLibHelper.ReadData(1, "NewConfirmPassword"));
+
+            }
+            catch (Exception msg)
+            {
+                Assert.Fail("Test failed at SignIn page", msg.Message);
+            }
+
+        }
+
+        public void ClickSave()
+        {
+            //Click save button
+            Wait.ElementExists(driver, "XPath", "//button[contains(text(),'Save')]", 50);
+
+            Save.Click();
+
+        }
+
+        public bool ValidateSuccessMessage()
+        {
+            Wait.ElementExists(driver, "XPath", "//div[contains(text(),'Password Changed Successfully')]", 100);
+
+            //validate password changed message is displayed
+            if (Message.Text == ExcelLibHelper.ReadData(1, "PasswordChangeMessage"))
+            {
+                //Console.WriteLine("Success message is displayed, test passed");
+                return true;
+            }
+            else
+            {
+                //Console.WriteLine("Success message is not displayed, test failed");
+                return false;
+            }
+        }
+
+    }
+}
---
+++ POM_Task2_DataDriven/Pages/SignInPage.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using POM_Task2_DataDriven.Utilities;
+
+namespace POM_Task2_DataDriven.Pages
+{
+    public class SignInPage
+    {
+        private readonly IWebDriver driver;
+
+        //creat a constructor
+        public SignInPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        
+
+        //page factory
+        IWebElement SignIn => driver.FindElement(By.XPath("//a[contains(text(),'Sign In')]"));
+        IWebElement EmailAddress => driver.FindElement(By.XPath("//input[@placeholder='Email address']"));
+        IWebElement Password => driver.FindElement(By.XPath("//input[@placeholder='Password']"));
+        IWebElement LoginButton => driver.FindElement(By.XPath("//button[contains(text(),'Login')]"));
+        IWebElement SignOut => driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/div[1]/div[2]/div/a[2]/button"));
+
+        // Methods Start Here
+        public void ClickSignIn()
+        {
+            Wait.ElementExists(driver, "XPath", "//a[contains(text(),'Sign In')]", 50);
+            //click sign in 
+            SignIn.Click();
+        }
+        public void ValidateYouAreAtLoginPage()
+        {
+            //Validate that I am in the login Page
+            Wait.ElementExists(driver, "XPath", "//button[contains(text(),'Login')]", 50);
+            bool isLoginPage =LoginButton.Displayed;
+            Assert.IsTrue(isLoginPage);
+        }
+        
+         
+        //public bool wValidateYouAreAtLoginPage()
+        //{
+        //    return LoginButton.Displayed;
+        //}
+
+        public void EnterEmailAndPassword(string email, string password)
+        {
+            try
+            {
+                //enter email address
+                EmailAddress.SendKeys(email);
+                Console.WriteLine("Enter Eamil Address" + email);
+
+                //eneter password
+                Password.SendKeys(password);
+                Console.WriteLine("Enter Password"+ password);
+            }
+            catch (Exception msg)
+            {
+                Assert.Fail("Test Failed at SignIn Page", msg.Message);
+            }
+            
+        }
+
+        public void ClickLoginButton()
+        {
+            //Click login Butoon
+            LoginButton.Click();
+        }
+
+        public bool ValidateYouAreLoggedInSuccessfully()
+        {
+            Wait.ElementExists(driver, "XPath", "//*[@id='account-profile-section']/div/div[1]/div[2]/div/a[2]/button", 10);
+            return SignOut.Displayed;
+        }
+
+
+        public void Login(string emailValue, string passwordValue)
+        {
+            ClickSignIn();
+            ValidateYouAreAtLoginPage();
+            EnterEmailAndPassword(emailValue, passwordValue);
+            ClickLoginButton();
+            bool IsLoggedIn = ValidateYouAreLoggedInSuccessfully();
+            Assert.IsTrue(IsLoggedIn);
+        }
+    }
+}
