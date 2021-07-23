74767e5a26ba7e5ae7bac3d55e3956c31ae99ba7
LOrence <lawrence.chandru@gmail.com>
2021-07-23 12:38:39 +1200

    Update

---
+++ POM_Task2_DataDriven/Pages/SearchPage.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using POM_Task2_DataDriven.Utilities;
+using static POM_Task2_DataDriven.Utilities.CommonMethods;
+
+namespace POM_Task2_DataDriven.Pages
+{
+    public class SearchPage
+    {
+        private readonly IWebDriver driver;
+        private readonly SignInPage signIn;
+
+        //page factory design pattern
+        IWebElement SearchIcon => driver.FindElement(By.XPath("//i[@class='search link icon']"));
+        IWebElement SearchSkillsBox => driver.FindElement(By.XPath("//section[@class='search-results']//input[@type='text'and @placeholder='Search skills']"));
+        IWebElement SearchedSkill => driver.FindElement(By.XPath("//p[contains(text(),'Badminton')]"));
+        IWebElement Online => driver.FindElement(By.XPath("//button[contains(text(),'Online')]"));
+
+        //Create a Constructor
+        public SearchPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            signIn = new SignInPage(driver);
+        }
+
+        // 1. searching a skill from all categories
+        public void SearchSkillsByAllCategories(string searchSkill)
+        {
+            signIn.Login(ExcelLibHelper.ReadData(1, "EmailAddress"), ExcelLibHelper.ReadData(1, "Password"));
+            ClickSearchIcon();
+            EnterSearchSkill(searchSkill);
+            ClickEnter();
+        }
+
+        // 2. searching a skill using filter
+        public void SearchSkillsByFilters()
+        {
+            signIn.Login(ExcelLibHelper.ReadData(1, "EmailAddress"), ExcelLibHelper.ReadData(1, "Password"));
+            ClickSearchIcon();
+            EnterSearchSkill(ExcelLibHelper.ReadData(1, "SearchSkillToAccept"));
+            ClickEnter();
+            ClickOnline();
+            bool isSearchResult = ValidateSearchResult(ExcelLibHelper.ReadData(1, "SearchSkillToAccept"));
+            Assert.IsTrue(isSearchResult);
+        }
+
+        public void ClickOnline()
+        {
+            //click online filter
+            Online.Click();
+        }
+
+        public bool ValidateSearchResult(string searchSkill)
+        {
+            Wait.ElementExists(driver, "XPath", "//p[contains(text(),'Badminton')]", 100);
+            //validate search result
+            if (SearchedSkill.Text == searchSkill)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void ClickEnter()
+        {
+            //click enter button
+            SearchSkillsBox.SendKeys(Keys.Enter);
+        }
+
+        public void EnterSearchSkill(string skill)
+        {
+            //enter skill to search
+            SearchSkillsBox.SendKeys(skill);
+        }
+
+        public void ClickSearchIcon()
+        {
+            //click search icon
+            SearchIcon.Click();
+        }
+    }
+}
---
+++ POM_Task2_DataDriven/Tests/SearchTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using AventStack.ExtentReports;
+using NUnit.Framework;
+using POM_Task2_DataDriven.Pages;
+using POM_Task2_DataDriven.Utilities;
+using static POM_Task2_DataDriven.Utilities.CommonMethods;
+
+namespace POM_Task2_DataDriven.Tests
+{
+    [TestFixture]
+
+    public class SearchTest : Driver
+    {
+        private readonly CommonMethods commonMethods;
+
+        public SearchTest()
+        {
+            commonMethods = new CommonMethods();
+        }
+
+        [Test]
+        [TestCaseSource(typeof(Driver), "BrowserToRunWith")]
+        public void SearchSkillsByAllCategoriesTest(string browserName)
+        {
+
+            try
+            {
+                test = extent.CreateTest(TestContext.CurrentContext.Test.Name).Info("Test Started");
+                test.Log(Status.Info, "SearchSkillsByAllCategories method is called");
+
+                Setup(browserName);
+                //Search Page Objects
+                SearchPage searchPageObj = new SearchPage(driver);
+                searchPageObj.SearchSkillsByAllCategories(ExcelLibHelper.ReadData(1, "SearchSkillToAccept"));
+
+                test.Log(Status.Pass, "Search skills by all categories is tested");
+                test.Pass("Test Passed");
+            }
+            catch (Exception e)
+            {
+
+                var mediaEntity = commonMethods.CaptureScreenshotAndReturnModel(TestContext.CurrentContext.Test.Name.Trim());
+                test.Log(Status.Fail, e.StackTrace.ToString());
+                test.Fail("Test Failed", mediaEntity);
+            }
+
+        }
+
+        [Test]
+        [TestCaseSource(typeof(Driver), "BrowserToRunWith")]
+        public void SearchSkillsByFiltersTest(string browserName)
+        {
+
+            try
+            {
+                test = extent.CreateTest(TestContext.CurrentContext.Test.Name).Info("Test Started");
+                test.Log(Status.Info, "SearchSkillsByFilters method is called");
+
+                Setup(browserName);
+                //Search Page Objects
+                SearchPage searchPageObj = new SearchPage(driver);
+                searchPageObj.SearchSkillsByFilters();
+
+                test.Log(Status.Pass, "Search skills by filters is tested");
+                test.Pass("Test Passed");
+            }
+            catch (Exception e)
+            {
+
+                var mediaEntity = commonMethods.CaptureScreenshotAndReturnModel(TestContext.CurrentContext.Test.Name.Trim());
+                test.Log(Status.Fail, e.StackTrace.ToString());
+                test.Fail("Test Failed", mediaEntity);
+            }
+
+        }
+    }
+}
